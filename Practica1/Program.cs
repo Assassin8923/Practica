e78a4a5dd3df9e8f6f4f86a2dc1bf8f668d637d4
chryniukrostyslav-dot <c.hryniuk.rostyslav@student.uzhnu.edu.ua>
2025-10-14 02:18:01 +0300

    новий файл:    .vscode/launch.json     новий файл:    .vscode/tasks.json 	новий файл:    Practica1.sln 	новий файл:    Practica1/Practica1.csproj 	новий файл:    Practica1/Program.cs 	новий файл:    Practica1/bin/Debug/net8.0/Practica1 	новий файл:    Practica1/bin/Debug/net8.0/Practica1.deps.json 	новий файл:    Practica1/bin/Debug/net8.0/Practica1.dll 	новий файл:    Practica1/bin/Debug/net8.0/Practica1.pdb 	новий файл:    Practica1/bin/Debug/net8.0/Practica1.runtimeconfig.json 	новий файл:    Practica1/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	новий файл:    Practica1/obj/Debug/net8.0/Practica1.AssemblyInfo.cs 	новий файл:    Practica1/obj/Debug/net8.0/Practica1.AssemblyInfoInputs.cache 	новий файл:    Practica1/obj/Debug/net8.0/Practica1.GeneratedMSBuildEditorConfig.editorconfig 	новий файл:    Practica1/obj/Debug/net8.0/Practica1.GlobalUsings.g.cs 	новий файл:    Practica1/obj/Debug/net8.0/Practica1.assets.cache     новий файл:    Practica1/obj/Debug/net8.0/Practica1.csproj.CoreCompileInputs.cache 	новий файл:    Practica1/obj/Debug/net8.0/Practica1.csproj.FileListAbsolute.txt 	новий файл:    Practica1/obj/Debug/net8.0/Practica1.dll 	новий файл:    Practica1/obj/Debug/net8.0/Practica1.genruntimeconfig.cache 	новий файл:    Practica1/obj/Debug/net8.0/Practica1.pdb 	новий файл:    Practica1/obj/Debug/net8.0/apphost     новий файл:    Practica1/obj/Debug/net8.0/ref/Practica1.dll 	новий файл:    Practica1/obj/Debug/net8.0/refint/Practica1.dll 	новий файл:    Practica1/obj/Practica1.csproj.nuget.dgspec.json 	новий файл:    Practica1/obj/Practica1.csproj.nuget.g.props 	новий файл:    Practica1/obj/Practica1.csproj.nuget.g.targets 	новий файл:    Practica1/obj/project.assets.json 	новий файл:    Practica1/obj/project.nuget.cache

---
+++ Practica1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double price1 = 500.00;
+        double price2 = 300.00;
+        double price3 = 700.00;
+        double prise4 = 150.00;
+        double prise5 = 250.00;
+
+        string product1 = "Подарунковий набір";
+        string product2 = "Імена чашка";
+        string product3 = "Букет";
+        string product4 = "Аромо свічка";
+        string product5 = "Шоколодний набір";
+
+        int unit1, unit2, unit3, unit4, unit5;
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Потрібно " + product1 + " (шт.)"); 
+        unit1 = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Потрібно " + product2 + " (шт.)"); 
+        unit2 = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Потрібно " + product3 + " (шт.)"); 
+        unit3 = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Потрібно " + product4 + " (шт.)"); 
+        unit4 = Convert.ToInt32(Console.ReadLine()); 
+        Console.WriteLine("Потрібно " + product5 + " (шт.)"); 
+        unit5 = Convert.ToInt32(Console.ReadLine());
+        Console.ResetColor();
+
+        double cost1 = price1 * unit1;
+        double cost2 = price2 * unit2;
+        double cost3 = price3 * unit3;
+        double cost4 = prise4 * unit4;
+        double cost5 = prise5 * unit5;  
+
+        double total = cost1 + cost2 + cost3 + cost4 + cost5;
+
+        total = Math.Round(total, 2);
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Деталізація замовлення:");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine("Вартість " + product1 + " за одиницю "  + price1 +  ". Загальна: " + cost1 + " грн.");
+        Console.WriteLine("Вартість " + product2 + " за одиницю "  + price2 +  ". Загальна: " + cost2 + " грн.");
+        Console.WriteLine("Вартість " + product3 + " за одиницю "  + price3 +  ". Загальна: " + cost3 + " грн."); 
+        Console.WriteLine("Вартість " + product4 + " за одиницю "  + prise4 +  ". Загальна: " + cost4 + " грн.");
+        Console.WriteLine("Вартість " + product5 + " за одиницю "  + prise5 +  ". Загальна: " + cost5 + " грн.");   
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Загальна вартість замовлення: " + total + " грн.");
+
+        Console.ReadKey();
+    }
+}
